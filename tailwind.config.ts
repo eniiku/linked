fea389aa43b8523545a6b722d0c14e297a45c77b
David Enikuomehin <enikuauth@gmail.com>
2023-09-20 09:38:20 +0100

    chore(*): setup assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)'],
+        clash: ['var(--font-clash-display)'],
+      },
+      colors: {
+        primary: 'rgb(var(--clr-pry) / 1)',
+        'primary-alt': 'rgb(var(--clr-pry-alt) / 1)',
+        purple: 'rgb(var(--clr-purple) / 1)',
+        pink: 'rgb(var(--clr-pink) / 1)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
